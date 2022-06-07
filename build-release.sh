2283805c63184d44dbb604334e8a65b40f2d9d21
Onno Lissenberg <olissenberg@rsm.nl>
2022-06-07 21:27:37 +0200

    Git clone Canvas source to project dir and copy into the container Update Rails to 6.1 Next release will be 2022-06-22.x

--- build-release.sh
+++ build-release.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-# ./build-release.sh release/2022-06-08.135
-# docker run --rm -ti canvas-lms-node:2022-06-08.135 /bin/bash
+# ./build-release.sh release/2022-06-22.01
+# docker run --rm -ti canvas-lms-node:2022-06-22.01 /bin/bash
 
 RELEASE="${1:-master}"
 TAG="${1:-latest}"
 
 cd canvas-lms-node || exit
+mkdir -p src/"${RELEASE}"
+git clone --depth 1 https://github.com/instructure/canvas-lms.git --single-branch --branch "${RELEASE}" src/"${RELEASE}"
 docker build --build-arg CANVAS_REVISION="${RELEASE}" -t canvas-lms-node:"${TAG#release/}" .
 
 cd ../canvas-lms-ruby || exit
