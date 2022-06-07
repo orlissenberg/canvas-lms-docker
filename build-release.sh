c72bfc41335d8f1d188d49a6eb3fcd6fbd792439
Onno Lissenberg <olissenberg@rsm.nl>
2022-06-07 22:38:41 +0200

    Create 2022-06-08 stable branch

--- build-release.sh
+++ build-release.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# ./build-release.sh release/2022-06-22.01
-# docker run --rm -ti canvas-lms-node:2022-06-22.01 /bin/bash
+# ./build-release.sh stable/2022-06-08
+# docker run --rm -ti canvas-lms-node:2022-06-08 /bin/bash
 
 RELEASE="${1:-master}"
-TAG="${1:-latest}"
+TAG=$(echo "${1:-latest}" | sed 's/stable\///g; s/release\///g')
 
 cd canvas-lms-node || exit
-mkdir -p src/"${RELEASE}"
-git clone --depth 1 https://github.com/instructure/canvas-lms.git --single-branch --branch "${RELEASE}" src/"${RELEASE}"
-docker build --build-arg CANVAS_REVISION="${RELEASE}" -t canvas-lms-node:"${TAG#release/}" .
+rm -rf src/"${RELEASE}" && mkdir -p src/"${RELEASE}"
+git clone --depth 1 https://github.com/instructure/canvas-lms.git --single-branch --branch "${RELEASE}" src/"${RELEASE}" || exit
+docker build --build-arg CANVAS_REVISION="${RELEASE}" -t canvas-lms-node:"${TAG}" .
 
 cd ../canvas-lms-ruby || exit
-docker build --build-arg TAG="${TAG#release/}" -t canvas-lms-ruby:"${TAG#release/}" .
+docker build --build-arg TAG="${TAG}" -t canvas-lms-ruby:"${TAG}" .
 
 cd ../canvas-lms || exit
-docker build --build-arg TAG="${TAG#release/}" -t orlissenberg/canvas-lms:"${TAG#release/}" .
+docker build --build-arg TAG="${TAG}" -t orlissenberg/canvas-lms:"${TAG}" .
 
-#docker push orlissenberg/canvas-lms:"${TAG#release/}"
+#docker push orlissenberg/canvas-lms:"${TAG}"
